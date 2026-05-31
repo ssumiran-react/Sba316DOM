8b525b6ecd14ea5207a0accb2ac9b6df960c4cca
ssumiran <ssumiran@yahoo.com>
2026-05-31 19:57:13 -0400

    Add html attribute validation

--- sba316dom.js
+++ sba316dom.js
@@ -139,7 +139,10 @@ function addAdminPanel() {
         h1.textContent = "Admin Panel";
 
         const panelDiv = document.createElement("div");
+        panelDiv.classList.add("row");
 
+////////////////////////////////////////////////////////////////////////////
+    // Game: Team Selection Div        
         const gameDiv = document.createElement("div");
 
         //Selecting Away Team and Home Team
@@ -333,16 +336,14 @@ function addAdminPanel() {
 /*
 Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
 
-Iterate over a collection of elements to accomplish some task.
-Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
 
-
-
-Include at least one form and/or input with HTML attribute validation.
-Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+Include at least one form and/or input with DOM event-based validation. 
+(This can be the same form or input as the one above, but should include event-based 
+validation in addition to the HTML attribute validation.)
 
 Done
-
+Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+Iterate over a collection of elements to accomplish some task.
 Cache at least one element using querySelector or querySelectorAll.
 Use at least two Browser Object Model (BOM) properties or methods.
 Cache at least one element using selectElementById.
@@ -352,4 +353,5 @@ Use appendChild and/or prepend to add new elements to the DOM.
 Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
 Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
 Modify at least one attribute of an element in response to user interaction.
+Include at least one form and/or input with HTML attribute validation.
 */
