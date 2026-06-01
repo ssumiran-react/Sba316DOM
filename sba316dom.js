f1bfd7203c48c3e6248c0559b6811f959c33e8af
ssumiran <ssumiran@yahoo.com>
2026-05-31 22:31:33 -0400

    delete comments

--- sba316dom.js
+++ sba316dom.js
@@ -464,27 +464,3 @@ function addAdminPanel() {
         window.alert("Error creating Admin Panel in: " + err.message);
     }
 }
-
-
-/*
-Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
-
-
-Include at least one form and/or input with DOM event-based validation. 
-(This can be the same form or input as the one above, but should include event-based 
-validation in addition to the HTML attribute validation.)
-
-Done
-Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
-Iterate over a collection of elements to accomplish some task.
-Cache at least one element using querySelector or querySelectorAll.
-Use at least two Browser Object Model (BOM) properties or methods.
-Cache at least one element using selectElementById.
-Register at least two different event listeners and create the associated event handler functions.
-Create at least one element using createElement.
-Use appendChild and/or prepend to add new elements to the DOM.
-Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
-Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
-Modify at least one attribute of an element in response to user interaction.
-Include at least one form and/or input with HTML attribute validation.
-*/
